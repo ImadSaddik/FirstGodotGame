d9a80efafaa4a59c858c55bd775d10a558684ad3
Imad Saddik <simad3647@gmail.com>
2025-01-14 15:47:31 +0100

    Added the isUpsideDown parameter to control the movement direction for upside down slimes.

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,16 +4,24 @@ extends Node2D
 @onready var leftRayCast: RayCast2D = $RayCastLeft
 @onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
 
+@export var isUpsideDown: bool = false
+
 const SPEED: int = 60
 var movementDirection = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if rightRayCast.is_colliding():
-		movementDirection = -1
+		if isUpsideDown:
+			movementDirection = 1
+		else:
+			movementDirection = -1
 		animatedSprite.flip_h = true
 	if leftRayCast.is_colliding():
-		movementDirection = 1
+		if isUpsideDown:
+			movementDirection = -1
+		else:
+			movementDirection = 1
 		animatedSprite.flip_h = false
 	
 	position.x += movementDirection * SPEED * delta
