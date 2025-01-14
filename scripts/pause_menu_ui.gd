8605405206ba6c4150b30de461ec71f3ec27889f
Imad Saddik <simad3647@gmail.com>
2025-01-14 13:09:35 +0100

    Implement the click on the resume button

--- scripts/pause_menu_ui.gd
+++ scripts/pause_menu_ui.gd
@@ -1,1 +1,12 @@
 extends Control
+
+@export var canvasLayer: CanvasLayer
+
+
+func _on_resume_button_pressed() -> void:
+	canvasLayer.hide()
+	
+
+
+func _on_exit_level_button_pressed() -> void:
+	pass
