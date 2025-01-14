97620c466e9c8b516f825af550f3e7b95dc7c871
Imad Saddik <simad3647@gmail.com>
2025-01-14 15:52:08 +0100

    Used inline if statement

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -9,19 +9,12 @@ extends Node2D
 const SPEED: int = 60
 var movementDirection = 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if rightRayCast.is_colliding():
-		if isUpsideDown:
-			movementDirection = 1
-		else:
-			movementDirection = -1
+		movementDirection = 1 if isUpsideDown else -1
 		animatedSprite.flip_h = true
 	if leftRayCast.is_colliding():
-		if isUpsideDown:
-			movementDirection = -1
-		else:
-			movementDirection = 1
+		movementDirection = -1 if isUpsideDown else 1
 		animatedSprite.flip_h = false
 	
 	position.x += movementDirection * SPEED * delta
