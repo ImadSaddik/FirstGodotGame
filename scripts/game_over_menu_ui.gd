a0e45abaee6f0555fb4c7816cc7660006a49c817
Imad Saddik <simad3647@gmail.com>
2025-01-14 18:35:43 +0100

    Added the game over UI scene

---
+++ scripts/game_over_menu_ui.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@export var canvasLayer: CanvasLayer
+
+func _on_retry_button_pressed() -> void:
+	canvasLayer.hide()
+	get_tree().reload_current_scene()
+
+
+func _on_exit_level_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/start_screen_ui.tscn")
