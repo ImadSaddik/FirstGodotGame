0271d24394b969c7f2cec1b114f41559871060d0
Imad Saddik <simad3647@gmail.com>
2025-01-14 18:22:26 +0100

    Implemented the exit level button

--- scripts/pause_menu_ui.gd
+++ scripts/pause_menu_ui.gd
@@ -5,8 +5,7 @@ extends Control
 
 func _on_resume_button_pressed() -> void:
 	canvasLayer.hide()
-	
 
 
 func _on_exit_level_button_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://scenes/start_screen_ui.tscn")
