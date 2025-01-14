b0f08dd22f64f98bdf730edce1cbb44d6a243b35
Imad Saddik <simad3647@gmail.com>
2025-01-14 08:49:10 +0100

    Finished updating the coins counter in the UI

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,12 @@
 extends Node
 
 @onready var scoreLabel: Label = $ScoreLabel
+@export var inGameUI: Control
 
 var score: int = 0
 
 func add_point():
 	score += 1
 	scoreLabel.text = "You collected " + str(score) + " coins."
+	inGameUI.update_coin_counter_label(score)
+	
