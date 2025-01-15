c623a90c5f4eeb7574661a29c17bdcb4f5e3580d
Imad Saddik <simad3647@gmail.com>
2025-01-15 08:57:13 +0100

    Added sliders to control the sounds of the game

--- scripts/start_screen_ui.gd
+++ scripts/start_screen_ui.gd
@@ -1,7 +1,16 @@
 extends Control
 
+@export var music_bus_name: String
+@export var sound_effects_bus_name: String
 @export var settingsView: Node
 @export var startMenuView: VBoxContainer
+
+var music_bus_index: int
+var sound_effects_bus_index: int
+
+func _ready() -> void:
+	music_bus_index = AudioServer.get_bus_index(music_bus_name)
+	sound_effects_bus_index = AudioServer.get_bus_index(sound_effects_bus_name)
 
 
 func _on_start_game_button_pressed() -> void:
@@ -10,23 +19,27 @@ func _on_start_game_button_pressed() -> void:
 
 func _on_settings_button_pressed() -> void:
 	startMenuView.hide()
-	show_child_inside_node(settingsView)
-	
-	
-func show_child_inside_node(node: Node) -> void:
-	for child in node.get_children(true):
-		child.show()
-		
-		
+	settingsView.show()
+
+
 func _on_exit_game_button_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_return_to_start_menu_button_pressed() -> void:
-	hide_child_inside_node(settingsView)
-	
-	
-func hide_child_inside_node(node: Node) -> void:
 	startMenuView.show()
-	for child in node.get_children(true):
-		child.hide()
+	settingsView.hide()
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		music_bus_index,
+		linear_to_db(value)
+	)
+
+
+func _on_sound_effects_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		sound_effects_bus_index,
+		linear_to_db(value)
+	)
