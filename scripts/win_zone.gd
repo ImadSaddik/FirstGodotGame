854b0afcb56a3a435bcc22ae75d0a96ed63cbe1d
Imad Saddik <simad3647@gmail.com>
2025-01-16 08:36:11 +0100

    Added the win zone

--- scripts/win_zone.gd
+++ scripts/win_zone.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
-signal player_died
+signal player_won
 
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You died!")
-	body.get_node("CollisionShape2D").queue_free()
+	print("WIN")
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
-	emit_signal("player_died")
+	emit_signal("player_won")
