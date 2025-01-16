0ba41f8be3329f3afd93121c4db975a3cd5df065
Imad Saddik <simad3647@gmail.com>
2025-01-16 08:41:11 +0100

    Added level win sound effect

--- scripts/win_zone.gd
+++ scripts/win_zone.gd
@@ -2,12 +2,13 @@ extends Area2D
 
 signal player_won
 
+@export var levelWonSoundEffect: AudioStreamPlayer2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("WIN")
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
 	emit_signal("player_won")
+	levelWonSoundEffect.play()
