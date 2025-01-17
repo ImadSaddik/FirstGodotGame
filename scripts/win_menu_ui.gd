f4dfb3e6b30f917a0d7e170dfb456bdcf8e02206
Imad Saddik <simad3647@gmail.com>
2025-01-17 23:35:23 +0100

    Added functionality for main menu button in the win menu

---
+++ scripts/win_menu_ui.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/start_screen_ui.tscn")
+
+
+func _on_next_level_button_pressed() -> void:
+	pass # Replace with function body.
