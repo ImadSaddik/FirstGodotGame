324a6af2ebdbf781f755b8649358852a63bc706f
Imad Saddik <simad3647@gmail.com>
2025-01-19 09:51:48 +0100

    Cleaned the script attached to the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,67 +19,19 @@ var jumpsPerformed: int = 0
 func _physics_process(delta: float) -> void:
 	if not canMakeAction:
 		return
-		
-	# Reset double jumps
+	
 	if is_on_floor():
 		reset_double_jumps()
-
-	# Add the gravity.
-	if not is_on_floor() and not isOnLadder:
-		velocity += get_gravity() * delta
-		# Handle double jump
-		if Input.is_action_just_pressed("jump") and canDoubleJump:
-			jumpsPerformed += 1
-			velocity.y = JUMP_VELOCITY
-			jumpSoundEffect.play()
-			
-			if jumpsPerformed > 1:
-				canDoubleJump = false
-		
-	# Handle ladder climbing
+		handle_jump()
+	
+	if is_in_air():
+		apply_gravity(delta)
+		handle_double_jump()
+	
 	if isOnLadder:
-		var vertical_direction = Input.get_axis("move_up", "move_down")
-		(vertical_direction)
-		velocity.y = vertical_direction * CLIMB_SPEED
-		playerAnimatedSprite.play("climb")
-
-	# Handle jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		canDoubleJump = true
-		jumpsPerformed += 1
-		velocity.y = JUMP_VELOCITY
-		jumpSoundEffect.play()
+		handle_ladder_climbing()
 		
-	# Hundle hit action
-	if Input.is_action_just_pressed("hit"):
-		sword.swing_sword()
-
-	# Get the input direction, it can be either -1, 0, 1.
-	var direction := Input.get_axis("move_left", "move_right")
-	
-	# Flip the sprite
-	if direction < 0:
-		playerAnimatedSprite.flip_h = true
-		sword.set_x_position(-SWORD_X_OFFSET_IN_PIXELS)
-	elif direction > 0:
-		playerAnimatedSprite.flip_h = false
-		sword.set_x_position(SWORD_X_OFFSET_IN_PIXELS)
-	
-	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			playerAnimatedSprite.play("idle")
-		else:
-			playerAnimatedSprite.play("run")
-	else:
-		playerAnimatedSprite.play("jump")
-	
-	# Move the player
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+	handle_actions()
 	move_and_slide()
 
 
@@ -90,6 +42,73 @@ func play_death_sound() -> void:
 func reset_double_jumps() -> void:
 	canDoubleJump = true
 	jumpsPerformed = 0
+
+
+func handle_jump() -> void:
+	if Input.is_action_just_pressed("jump"):
+		jumpsPerformed += 1
+		velocity.y = JUMP_VELOCITY
+		jumpSoundEffect.play()
+
+
+func is_in_air() -> bool:
+	return not is_on_floor() and not isOnLadder
+
+
+func apply_gravity(delta: float) -> void:
+	velocity += get_gravity() * delta
+
+
+func handle_double_jump() -> void:
+	if not canDoubleJump:
+		return
+	
+	handle_jump()
+	if jumpsPerformed > 1:
+		canDoubleJump = false
+
+
+func handle_ladder_climbing() -> void:
+	var vertical_direction = Input.get_axis("move_up", "move_down")
+	velocity.y = vertical_direction * CLIMB_SPEED
+	playerAnimatedSprite.play("climb")
+
+
+func handle_actions() -> void:
+	if Input.is_action_just_pressed("hit"):
+		sword.swing_sword()
+	
+	# Get the input direction, it can be either -1, 0, 1.
+	var direction := Input.get_axis("move_left", "move_right")
+	flip_player_based_on_direction(direction)
+	move_player_in_direction(direction)
+	play_animations(direction)
+
+
+func flip_player_based_on_direction(direction: float) -> void:
+	if direction < 0:
+		playerAnimatedSprite.flip_h = true
+		sword.set_x_position(-SWORD_X_OFFSET_IN_PIXELS)
+	elif direction > 0:
+		playerAnimatedSprite.flip_h = false
+		sword.set_x_position(SWORD_X_OFFSET_IN_PIXELS)
+
+
+func move_player_in_direction(direction: float) -> void:
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+func play_animations(direction: float) -> void:
+	if is_on_floor():
+		if direction == 0:
+			playerAnimatedSprite.play("idle")
+		else:
+			playerAnimatedSprite.play("run")
+	else:
+		playerAnimatedSprite.play("jump")
 
 
 func _on_timer_timeout() -> void:
