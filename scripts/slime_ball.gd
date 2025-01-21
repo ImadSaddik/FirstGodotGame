0062d9e90bcedc026cf3bffc307ec81ea8556339
Imad Saddik <simad3647@gmail.com>
2025-01-21 21:17:10 +0100

    Made the slime ball emit the player died signal to slime

--- scripts/slime_ball.gd
+++ scripts/slime_ball.gd
@@ -1,7 +1,14 @@
 extends RigidBody2D
 
+signal player_died
+
 @export var timer: Timer
 
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_player_died() -> void:
+	print("SLIME BALL - Player died")
+	emit_signal("player_died")
