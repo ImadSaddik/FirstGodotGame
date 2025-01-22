b8f3020405812d204412eed087d1a0b3388c23c5
Imad Saddik <simad3647@gmail.com>
2025-01-22 08:09:12 +0100

    Destroyed the ball when it hits the ground

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
 signal player_died
+signal ball_hit_ground
 
 func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		handle_player_case(body)
+		emit_signal("player_died")
+	elif body is TileMap:
+		emit_signal("ball_hit_ground")
+
+
+func handle_player_case(body: Node2D) -> void:
 	body.hide()
 	body.isDead = true
 	
 	var collisionShape2D = body.get_node("CollisionShape2D")
 	collisionShape2D.queue_free()
-	
-	emit_signal("player_died")
--- scripts/slime_ball.gd
+++ scripts/slime_ball.gd
@@ -11,3 +11,11 @@ func _on_timer_timeout() -> void:
 
 func _on_player_died() -> void:
 	emit_signal("player_died")
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Hit the ground")
+
+
+func _on_ball_hit_ground() -> void:
+	queue_free()
